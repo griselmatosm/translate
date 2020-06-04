b1ef0be0ef1eaa6a4add5ba501cd5aeb9186bfdb
Grisel Matos <griselmatosm@gmail.com>
2020-06-05 00:01:19 +0200

    Add docs folder

---
+++ docs/precache-manifest.afc4bc62829b02f2ed6e04c9b1a4d91e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaadb6511fea077b1e8ffefb21181b63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12cb610829d03f35a136",
+    "url": "./static/js/2.4a8c354c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4a8c354c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b6c03f43bd18878e8f",
+    "url": "./static/js/main.6ef18909.chunk.js"
+  },
+  {
+    "revision": "690f9d9258e2adcf2f9a",
+    "url": "./static/js/runtime-main.fae778b2.js"
+  }
+]);
