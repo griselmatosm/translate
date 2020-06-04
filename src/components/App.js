9d89ef7cc9ad76b77fa8ae0b23f017c2d7c75d30
Grisel Matos <griselmatosm@gmail.com>
2020-06-04 23:57:31 +0200

    Accesing data with Consumers

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -16,9 +17,11 @@ class App extends React.Component {
           <i className="flag uk" onClick={() => this.onLanguageChange('english')} />
           <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value={'red'}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
