14bb6c664aebbac53cb6c52280f813a8ee17b79f
Grisel Matos <griselmatosm@gmail.com>
2020-06-05 09:36:49 +0200

    Connecting the Field and the Button with the store

--- src/components/Field.js
+++ src/components/Field.js
@@ -4,7 +4,7 @@ import LanguageContext from '../contexts/LanguageContext';
 class Field extends React.Component {
   static contextType = LanguageContext;
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Nombre';
+    const text = this.context.language === 'english' ? 'Name' : 'Nombre';
     return (
       <div className="ui field">
         <label>{text}</label>
